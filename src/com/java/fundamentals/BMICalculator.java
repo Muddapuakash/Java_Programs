7a9826f3a58b62c733f4bd13a84a4ed0eb8c0faa
MuddapuAkash <akashmuddapu@gmail.com>
2026-02-11 20:31:15 +0530

    Day 2 BMICalculator

---
+++ src/com/java/fundamentals/BMICalculator.java
@@ -0,0 +1,22 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter weight (kg): ");
+        double weight = scanner.nextDouble();
+
+        System.out.print("Enter height (m): ");
+        double height = scanner.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        System.out.printf("Your BMI: %.2f%n", bmi);
+
+        scanner.close();
+
+    }
+
+}
